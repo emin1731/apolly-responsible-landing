69fd9cafa42613c190345b38d0b2b249b54802db
emin1731 <emin1731@icloud.com>
2023-09-02 00:20:14 +0400

    Add customers, blog sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,3 +32,37 @@ const swiper = new Swiper('.swiper', {
     //     }
     //   }
 })
+
+
+const customersSwiper = new Swiper('.customers-swiper', {
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    
+    effect: 'cards',
+    cardsEffect: {
+        perSlideOffset: 19,
+        perSlideRotate: 0,
+        slideShadows: false,
+        rotate: false,
+    },
+    keyboard: {
+        enabled: true
+    },
+
+    // pagination: {
+    //     el: ".customers-swiper-pagination",
+    //     clickable: true,
+    // },   
+
+    // breakpoints: {
+    //     460: {
+    //     },
+    //     768: {
+    //     },
+    //     1024: {
+    //     },
+    //     1600: {
+    //     }
+    //   }
+})
